73145b67d9ceaf73f16f0419a88cc33d34268ef1
Christopher Gonzalez <crg167@gmail.com>
2018-09-11 16:39:57 -0700

    remove todos from files

--- licensing-header.py
+++ licensing-header.py
@@ -1,11 +1,6 @@
 #!/usr/bin/env python
 # encoding: utf-8
 
-### TODO section ###
-# - add support for yaml
-# - should consider an ignore flag?
-# - update readme
-
 import os
 import re
 import argparse
